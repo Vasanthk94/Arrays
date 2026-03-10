d4057d2615dfdb9c4f890ce90ee8438407181f93
Vasanth K <vasanthkamaraj942003@gmail.com>
2026-03-10 17:57:49 +0530

    Add files via upload

---
+++ LeftRotate.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class LeftRotate{
+	public static void main(String[] args) {
+		int[]arr={1,2,3,4,5};
+		System.out.println(Arrays.toString(rotate(arr)));
+	}
+	public static int[] rotate(int[]arr){
+		int key=2;
+		int[]b=new int[arr.length];
+		int j=0;
+		for (int i=key;i<arr.length ;i++ ) 
+		{
+			b[j++]=arr[i];
+		}
+		for (int i=0;i<key;i++) 
+		{
+			b[j++]=arr[i];
+		}
+		return b;
+	}
+}
